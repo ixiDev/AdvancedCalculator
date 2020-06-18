75d7f1f7bd26343a0f29c7afeb9ebb5e5f40bbae
ixiDev <feedback.mrzero@gmail.com>
2020-06-18 16:33:31 +0100

    Initial commit

---
+++ app/src/main/java/example/ixidev/advancedcalculator/MainActivity.kt
@@ -0,0 +1,23 @@
+package example.ixidev.advancedcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        inputTextView.hideKeyBoard()
+    }
+
+    fun onClearClick(view: View) {}
+    fun onNumberClick(view: View) {}
+    fun onOperationClick(view: View) {}
+    fun onEqualsButtonClick(view: View) {}
+}
---
+++ app/src/main/java/example/ixidev/advancedcalculator/Utils.kt
@@ -0,0 +1,13 @@
+package example.ixidev.advancedcalculator
+
+import android.text.InputType
+import android.widget.EditText
+
+/**
+ * Created by ABDELMAJID ID ALI on 18/06/2020.
+ * Email : abdelmajid.idali@gmail.com
+ * Github : https://github.com/ixiDev
+ */
+fun EditText.hideKeyBoard() {
+    this.setRawInputType(InputType.TYPE_NULL)
+}
